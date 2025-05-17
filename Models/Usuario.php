87640038e562cf58b78e52d85732d1349ff07257
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-05-17 20:45:41 -0300

    final da página usuários

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -8,19 +8,19 @@ final class Usuario
     private string $nome;
     private string $email;
     private string $senha;
-    private string $dataDeCadastro;
+    private string $datadecadastro;
 
     public function __construct(
         string $nome,
         string $email,
         string $senha,
-        string $DataDeCadastro,
+        string $datadecadastro,
         ?int $id = null
     ){
         $this->setNome($nome);
         $this->setEmail($email);
         $this->setSenha($senha);
-        $this->setDataDeCadastro($DataDeCadastro);
+        $this->setdatadecadastro($datadecadastro);
         $this->setId($id);
     }
 
@@ -28,3 +28,40 @@ final class Usuario
     {
         return $this->nome;
     }
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function getSenha(): string
+    {
+        return $this->senha;
+    }
+    public function getddatadecadastro(): string 
+    {
+        return $this->datadecadastro;
+    }
+
+    private function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    } 
+
+    private function setEmail(string $email): void 
+    {
+        $this->email = $email;
+    }
+
+    private function setSenha(string $senha): void
+    {
+        $this->senha = $senha;
+    }
+
+    private function setdatadecadastro(string $datadecadastro): void 
+    {
+        $this->datadecadastro = $datadecadastro;
+    }
+    private function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+}
